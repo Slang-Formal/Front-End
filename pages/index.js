e43819ad23372a10cdc23669ccc5d83ca123d019
borizer43 <madhav56rg@gmail.com>
2023-03-27 14:23:04 -0700

    Added Chakra UI Components

--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,55 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Script from 'next/script'
 import { FormEvent } from 'react'
+import { useState } from 'react'
+import { FormLabel, Textarea, Text } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [value, setValue] = useState('')
+
+  function handleInputChange(event) {
+    setValue(event.target.value)
+  }
 
   return (
     
     <>
-      <h1 style={{ color: 'azure' }}>  Slang to Formal Translator </h1>
+      <Heading style={{ color: 'black' }} size = '4xl' textAlign = 'center'>  Slang to Formal Translator </Heading>
 
-      <form action = '/api/form' method = 'post'> 
-        <label style ={{display: 'block'}} for='sentence'>Enter Sentence:</label>
-        <div><textarea id="sentence" name="sentence" rows="4" cols="50"></textarea></div>
-        <input type = 'submit' value = 'Submit'></input>
-      </form>
+      <FormLabel htmlFor="textarea" fontWeight = 'bold' fontSize ='30px' textAlign= 'left' marginLeft='7' mb ='0px'>
+        Input:
+      </FormLabel>
+      <Textarea
+        value={value}
+        onChange={handleInputChange}
+        placeholder='Enter a sentence here'
+        size='sm'
+        id='textarea'
+        name='textarea'
+        padding="10px"
+        margin="10px"
+        width="80%"
+        height="200px"
+        marginLeft='7'
+      />
           
-      <label style = {{display: 'block'}} for="textbox">
+      <FormLabel  htmlFor="textbox" fontWeight = 'bold' fontSize ='30px' marginLeft='7' mb = '0px'>
         Output:
-      </label>
+      </FormLabel>
+      <Textarea
+        value ={value}
+        size='sm'
+        id='textbox'
+        name='textbox'
+        padding="10px"
+        margin="10px"
+        width="80%"
+        height="200px"
+        marginLeft='7'
+      />
           
-      <textarea id="textbox" name="textbox" rows="4" cols="50"></textarea>
-
     </>
   )
 }
