e4ba5f666140db7ff85950345f084d43e81de364
borizer43 <madhav56rg@gmail.com>
2023-03-28 12:07:02 -0700

    Added a button

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,23 @@ import Script from 'next/script'
 import { FormEvent } from 'react'
 import { useState } from 'react'
 import { FormLabel, Textarea, Text } from '@chakra-ui/react'
-import { Heading } from '@chakra-ui/react'
+import { Heading, Button, ButtonGroup } from '@chakra-ui/react'
 
 
 export default function Home() {
   const [value, setValue] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [text, setText] = useState('')
+
+
+  const handleClick = () => {
+    setIsLoading(true);
+    setTimeout( () => {
+      setText(value)
+      setIsLoading(false);
+    }, '1000' ) 
+  };
+
 
   function handleInputChange(event) {
     setValue(event.target.value)
@@ -37,12 +49,21 @@ export default function Home() {
         height="200px"
         marginLeft='7'
       />
+      <Button 
+      colorScheme='gray' 
+      size='md'
+      textAlign= 'left' 
+      marginLeft='7'
+      isLoading = {isLoading}
+      onClick ={handleClick}
+      loadingText='Translating'
+      >{isLoading? 'Translating':'Translate'}</Button>
           
       <FormLabel  htmlFor="textbox" fontWeight = 'bold' fontSize ='30px' marginLeft='7' mb = '0px'>
         Output:
       </FormLabel>
       <Textarea
-        value ={value}
+        value ={text}
         size='sm'
         id='textbox'
         name='textbox'
@@ -51,6 +72,7 @@ export default function Home() {
         width="80%"
         height="200px"
         marginLeft='7'
+        isReadOnly
       />
           
     </>
