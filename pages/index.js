7c8a6c1e4cfde17552998189e2641d5a7b3420ee
Madhav Viswesvaran <68714586+madhav-vis@users.noreply.github.com>
2023-04-10 21:11:24 -0700

    Add files via upload
    Positioned Button properly, working on text gradient animation

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,30 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Script from 'next/script'
 import { FormEvent } from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FormLabel, Textarea, Text } from '@chakra-ui/react'
-import { Heading, Button, ButtonGroup } from '@chakra-ui/react'
+import { Heading, Button, ButtonGroup, useColorMode, Box } from '@chakra-ui/react'
 
 
 export default function Home() {
   const [value, setValue] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const [text, setText] = useState('')
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [colorIndex, setColorIndex] = useState(0);
+  const colors = ["red.200", "green.200", "blue.200"];
+  const ColorCycle = colors[colorIndex];
+const [hue, setHue] = useState(200);
+  
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setHue(hue => (hue + 1) % 360);
+      }, 1000);
+      return () => clearInterval(intervalId);
+    }, []);
+  
+    const gradient = `linear-gradient(to right, hsl(${hue}, 100%, 50%), hsl(${(hue + 120) % 360}, 100%, 50%), hsl(${(hue + 240) % 360}, 100%, 50%))`;
+  
 
 
   const handleClick = () => {
@@ -23,6 +38,14 @@ export default function Home() {
     }, '1000' ) 
   };
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setColorIndex((colorIndex) => (colorIndex + 1) % colors.length);
+    }, 1000);
+  
+    return () => clearInterval(intervalId);
+  }, []);
+
 
   function handleInputChange(event) {
     setValue(event.target.value)
@@ -31,8 +54,17 @@ export default function Home() {
   return (
     
     <>
-      <Heading style={{ color: 'black' }} size = '4xl' textAlign = 'center'>  Slang to Formal Translator </Heading>
-
+      <Heading 
+      size = '4xl'
+      bg={gradient}
+      backgroundClip="text"
+      textFillColor="transparent"
+      _hover={{ bg: gradient, textFillColor: "white" }}
+      transition="background 0.3s ease-in-out, text-fill-color 0.3s ease-in-out"
+      textAlign = 'center'>  
+      Slang to Formal Translator 
+      </Heading>
+      <div>
       <FormLabel htmlFor="textarea" fontWeight = 'bold' fontSize ='30px' textAlign= 'left' marginLeft='7' mb ='0px'>
         Input:
       </FormLabel>
@@ -49,6 +81,10 @@ export default function Home() {
         height="200px"
         marginLeft='7'
       />
+      </div>
+
+
+      <div>
       <Button 
       colorScheme='gray' 
       size='md'
@@ -58,6 +94,8 @@ export default function Home() {
       onClick ={handleClick}
       loadingText='Translating'
       >{isLoading? 'Translating':'Translate'}</Button>
+      </div>
+      
           
       <FormLabel  htmlFor="textbox" fontWeight = 'bold' fontSize ='30px' marginLeft='7' mb = '0px'>
         Output:
